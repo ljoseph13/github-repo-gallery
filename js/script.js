89e5fd87b1823459b4d0cb08b92ff8617f3e8b1d
ljoseph13 <lenise.joseph@gmail.com>
2022-12-27 03:18:20 -0500

    Added user and repo API info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Overview with username variables
 const overview = document.querySelector(".overview");
 const username = "ljoseph13";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
     const userInfo = await fetch(`https://api.github.com/users/ljoseph13`);
@@ -22,4 +23,20 @@ const displayUserInfo = function (data) {
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
 overview.append(div);
+gitRepos();
+};
+
+const gitRepos = async function () {
+    const fetchRepos = await fetch(`https://api.github.com/users/ljoseph13/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
